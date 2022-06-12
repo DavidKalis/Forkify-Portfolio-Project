d6fbaf8f2785821dce57fefdbf7b79551e63afe9
DavidKalis <dawek977@gmail.com>
2022-06-12 05:45:49 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c8b8bea0-83be-4b82-a9df-2ec1d1d47e7e';
+export const MODAL_CLOSE_SEC = 2.5;
